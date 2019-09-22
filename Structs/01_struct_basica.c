f119d7c6bd9ca8d7d1dd58f5862779d91ee34839
Laura Petrola <46495368+ptrlauu@users.noreply.github.com>
2019-09-21 21:54:52 -0300

    Update 01_struct_basica.c

--- Structs/01_struct_basica.c
+++ Structs/01_struct_basica.c
@@ -1,1 +1,18 @@
+#include <stdio.h> //scanf printf
 
+//utilizando structs
+typedef struct ponto 
+{
+    float x;
+    float y;
+}ponto;
+
+
+int main(void)
+{
+    struct ponto p;
+    printf("Digite as coordenadas do ponto (x,y): ");
+    scanf("%f %f",&p.x,&p.y); //acessando x e y da struct
+    printf("O ponto fornecido foi: (%.2f,%.2f))\n",p.x,p.y);
+    return 0;
+}
